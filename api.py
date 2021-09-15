dbfc57158912af4448ad940275ad23a6f914ba3f
Desktop <ivans200188@gmail.com>
2021-09-15 14:25:46 +0300

    api

--- api.py
+++ api.py
@@ -1,22 +1,46 @@
 from db_setup import db_session
 from db_models import *
-from flask import jsonify
+from flask import jsonify, request, redirect, url_for
 from flask_restful import Resource, reqparse
 
 parser = reqparse.RequestParser()
 parser.add_argument('client_id', type=int, help="Enter the client id")
 parser.add_argument('order_id', type=int, help="Enter the client id")
+parser.add_argument('client_name', type=int, help="Enter the client id")
+parser.add_argument('client_surname', type=int, help="Enter the client id")
+parser.add_argument('client_email', type=int, help="Enter the client id")
 
 
 class ClientWrapper(Resource):
     def __init__(self):
-        self._cid = parser.parse_args().get('client_id')
+        self._id = parser.parse_args().get('client_id')
+        self._name = parser.parse_args().get('client_name')
+        self._surname = parser.parse_args().get('client_surname')
+        self._email = parser.parse_args().get('client_email')
 
     def get(self):
-        if self._cid:
-            return jsonify(db_session.query(Client).get(self._cid).serialize())
+        if self._id:
+            return jsonify(db_session.query(Client).get(self._id).serialize())
         else:
             return {"clients": list(map(lambda cl: cl.serialize(), db_session.query(Client).all()))}
+
+    def post(self):
+        try:
+            if self._name and self._surname and self._email:
+                c = Client(self._name, self._surname, self._email)
+                db_session.add(c)
+                db_session.flush()
+
+                o = Order(c.id, c.name, 0)
+                db_session.add(o)
+                db_session.commit()
+                print("Добавлено")
+            else:
+                raise ValueError
+        except:
+            db_session.rollback()
+            print("Ошибка добавления в БД")
+        return redirect(url_for('register'))
 
 
 class OrderWrapper(Resource):
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask_restful import Api
 from flask import Flask, render_template, request, redirect, url_for
 from api import ClientWrapper, OrderWrapper
 from db_setup import db_session, init_db
-from db_models import *
 import os
 
 app = Flask(__name__)
@@ -33,25 +32,9 @@ def dynamic_tree():
     return render_template("layouts/dynamic.html", title="Бинарное дерево")
 
 
-@app.route('/register', methods=("POST", "GET"))
+@app.route('/register')
 def register():
-    if request.method == "POST":
-        # здесь должна быть проверка корректности введенных данных
-        try:
-            c = Client(request.form['name'], request.form['surname'], request.form['email'])
-            db_session.add(c)
-            db_session.flush()
-
-            o = Order(c.id, c.name, 0)
-            db_session.add(o)
-            db_session.commit()
-            print("Добавлено")
-        except:
-            db_session.rollback()
-            print("Ошибка добавления в БД")
-        return redirect(url_for('register'))
-    if request.method == "GET":
-        return render_template("layouts/register.html", title="Регистрация")
+    return render_template("layouts/register.html", title="Регистрация")
 
 
 @app.teardown_appcontext
