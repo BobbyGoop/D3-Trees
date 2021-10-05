77bf281224e245b719a336777dc1a842a636c012
Desktop <ivans200188@gmail.com>
2021-10-05 19:09:52 +0300

    changes

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,17 @@
 import os
 from flask import Flask
 from flask_restful import Api
-from src.views.contents.routes import initialise_contents
-from src.views.api.routes import initialise_resources
-from src.database.setup import initialise_db
 
+from src.database.setup import db
+
+from src.views.api.ClientWrapper import ClientWrapper
+from src.views.api.OrderWrapper import OrderWrapper
+
+from src.views.contents.home import home
+from src.views.contents.metro import metro
+from src.views.contents.register import register
+from src.views.contents.resources import resources
+from src.views.contents.tree import tree
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/store.db'
@@ -14,7 +21,22 @@ app._static_folder = os.path.abspath("src/static/")
 api = Api(app)
 
 if __name__ == '__main__':
-    initialise_db(app)
-    initialise_contents(app)
-    initialise_resources(api)
+
+    # Setting up database
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+    # Registering blueprints
+    app.register_blueprint(home)
+    app.register_blueprint(register)
+    app.register_blueprint(resources)
+    app.register_blueprint(tree)
+    app.register_blueprint(metro)
+
+    # Adding API resources
+    api.add_resource(ClientWrapper, '/api/clients/', '/api/clients/<int:client_id>')
+    api.add_resource(OrderWrapper, '/api/orders/', '/api/orders/<int:client_id>', '/api/order/<int:order_id>')
+
+    # RUNNING
     app.run(debug=True, host='127.0.0.1', port=5000)
--- src/database/setup.py
+++ src/database/setup.py
@@ -1,9 +1,3 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
-
-def initialise_db(app):
-	db.init_app(app)
-	with app.app_context():
-		db.create_all()
